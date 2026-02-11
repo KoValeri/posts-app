356ffb589705e734de014cdaf00ff5ed35fcfbe9
Kovalenko Valeria <VKavalenka@ibagroup.eu>
2026-02-11 19:46:42 +0300

    [Fix] Fix font size for different screens - PostCard.tsx

--- src/pages/Home/PostCard.tsx
+++ src/pages/Home/PostCard.tsx
@@ -7,15 +7,15 @@ const PostCard: React.FC<Post> = ({ id, title, body, tags, reactions, views }) =
     <Link className="p-5" to={ROUTES.POST} params={{ id }}>
       <div className="p-5 border rounded-lg shadow-sm bg-white space-y-3 hover:bg-gray-50 transition-colors cursor-pointer">
         
-        <h2 className="text-lg font-bold">{title}</h2>
-        <p className="text-gray-700 truncate">{body}</p>
+        <h2 className="text-lg md:text-xl lg:text-xl font-bold">{title}</h2>
+        <p className="text-sm md:text-base lg:text-base text-gray-700 truncate">{body}</p>
 
 
         <div className="flex flex-wrap gap-2">
           {tags.map(tag => (
             <span
               key={tag}
-              className="text-xs bg-gray-100 text-gray-700 px-2 py-1 rounded-full"
+              className="text-xs md:text-sm bg-gray-100 text-gray-700 px-2 py-1 rounded-full"
             >
               #{tag}
             </span>
@@ -25,15 +25,15 @@ const PostCard: React.FC<Post> = ({ id, title, body, tags, reactions, views }) =
         <div className="flex justify-between items-center pt-2">
 
           <div className="flex gap-2">
-            <span className="text-sm bg-gray-200 px-3 py-1 rounded-full">
+            <span className="text-xs md:text-sm bg-gray-200 px-3 py-1 rounded-full">
               👍 {reactions.likes}
             </span>
-            <span className="text-sm bg-gray-200 px-3 py-1 rounded-full">
+            <span className="text-xs md:text-sm bg-gray-200 px-3 py-1 rounded-full">
               👎 {reactions.dislikes}
             </span>
           </div>
 
-          <span className="text-sm bg-gray-200 px-3 py-1 rounded-full">
+          <span className="text-xs md:text-sm bg-gray-200 px-3 py-1 rounded-full">
             👁 {views}
           </span>
         </div>
